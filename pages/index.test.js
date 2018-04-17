960047a8e62f1d0e0ea9d77b5e2f905cdfc092be
Kirill Konshin <kirill.konshin@gmail.com>
2018-04-16 21:57:27 -0700

    Now

--- pages/index.test.js
+++ pages/index.test.js
@@ -6,11 +6,13 @@ describe('Basic integration', () => {
 
     it('shows the page', async () => {
         await openPage();
+        await page.content();
         await expect(page).toMatch('Hello, octocat!Click');
     });
 
     it('clicks the button', async () => {
         await openPage();
+        await page.content();
         await expect(page).toClick('button', {text: 'Click'});
         await expect(page).toMatch('Hello, octocat!ClickClicked');
     });
